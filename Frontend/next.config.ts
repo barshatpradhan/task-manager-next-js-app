c81fb17262d45ada6e855dc5ed4f20ddeb619104
Barshat Pradhan <94312507+barshatpradhan@users.noreply.github.com>
2026-01-06 00:51:34 -0500

    update next.config file

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -4,33 +4,33 @@
 
 // module.exports = nextConfig
 
-// import type { NextConfig } from 'next';
-
-// const nextConfig: NextConfig = {
-//   reactStrictMode: true,
-  
-//   env: {
-//     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-//   },
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    // Fallback to a string to prevent "undefined" error during build
-    const backendUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
-    
-    return [
-      {
-        source: '/api/proxy/:path*',
-        destination: `${backendUrl}/api/:path*`,
-      },
-    ];
+  
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 };
 
 export default nextConfig;
+
+// import type { NextConfig } from 'next';
+
+// const nextConfig: NextConfig = {
+//   reactStrictMode: true,
+//   async rewrites() {
+//     // Fallback to a string to prevent "undefined" error during build
+//     const backendUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+    
+//     return [
+//       {
+//         source: '/api/proxy/:path*',
+//         destination: `${backendUrl}/api/:path*`,
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
