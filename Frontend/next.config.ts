2628853ad735a7fc82d5badc9825f4b38a661d5c
Barshat Pradhan <94312507+barshatpradhan@users.noreply.github.com>
2026-01-06 00:42:59 -0500

    edit next.config.ts file

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -4,13 +4,31 @@
 
 // module.exports = nextConfig
 
+// import type { NextConfig } from 'next';
+
+// const nextConfig: NextConfig = {
+//   reactStrictMode: true,
+  
+//   env: {
+//     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  async rewrites() {
+    return [
+      {
+        // This takes any call to /api/proxy/... 
+        source: '/api/proxy/:path*',
+        // And sends it to your Render backend URL
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+      },
+    ];
   },
 };
 
